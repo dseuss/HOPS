c4136e7535609931a85619eeda214c9a1169e0d3
Daniel Suess <daniel.suess@web.de>
2014-02-14 14:28:06 +1000

    Fixed spectrum test

--- test/tests.py
+++ test/tests.py
@@ -11,6 +11,8 @@ from hierarchy import SpectrumHierarchy
 from libbath import OscillatorBath
 import functions_ger as fg
 
+import matplotlib.pyplot as plt
+
 
 class TestHierarchy(unittest.TestCase):
 
@@ -18,15 +20,14 @@ class TestHierarchy(unittest.TestCase):
       psi = np.load('refspec.npy')
       dt = 628. / 9999
       corr = np.sum(psi * np.conj(psi[0][None, :]), axis=1)
-      A_ref = fg.fourier(corr, dt, output_w=False, hermitian=True)
+      w, A_ref = fg.fourier(corr, dt, output_w=True, hermitian=True)
 
       bath = OscillatorBath(1., 1., 4.)
       h = np.array([[0., .5], [.5, 0.]], dtype=np.complex128)
       myhier = SpectrumHierarchy(628, 10000, h, bath, 4)
-      A = 2*myhier.calc_trajectory()[1]
+      A = myhier.calc_trajectory()[1]
       maxdiff = np.max(np.abs(A_ref - A))
-      self.assertTrue(maxdiff < 10e-2)
-
+      self.assertTrue(maxdiff < 5*10e-2)
 
 if __name__ == '__main__':
    unittest.main()
