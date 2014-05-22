d459e868c4e0885e19c226f258c55fda72be6ea8
Daniel Suess <daniel.suess@web.de>
2014-05-22 15:05:26 +0200

    Add per-mode-cutoff to hstruct

--- src/hstruct.f90
+++ src/hstruct.f90
@@ -1,3 +1,8 @@
+! Module to manage the structure of HOPS. The hierarchy structure is determined
+! by the set of all valid vector indices combined with an enumeration of the
+! latter.
+!
+! The valid entries are determined by the truncation condition.
 module hstruct
 use system
 use hstructtab, only: HStructureTable
@@ -16,13 +21,15 @@ type, public :: HStructure
 
    type(HStructureTable) :: intind_
    integer, allocatable :: &
+         cutoff_(:), &
          vecind_(:, :), &
          indab_(:, :), &
          indbl_(:, :)
 
    contains
    private
-   procedure :: recursive_indices
+   procedure :: recursive_indices_
+   procedure :: add_entry_
 
    procedure, public :: init
    procedure, public :: free
@@ -36,11 +43,10 @@ end type HStructure
 
 contains
 
-recursive subroutine recursive_indices(self, counter, at_mode, indices, currpop)
+recursive subroutine recursive_indices_(self, at_mode, indices, currpop)
    implicit none
 
    class(HStructure)      :: self
-   integer, intent(inout) :: counter
    integer, intent(in)    :: at_mode
    integer, intent(in)    :: indices(self%modes_)
    integer, intent(in)    :: currpop
@@ -49,11 +55,10 @@ recursive subroutine recursive_indices(self, counter, at_mode, indices, currpop)
 
    ! first we treat the at_mode - unpopulated case
    if (at_mode >= self%modes_) then
-      counter = counter + 1
-      self%vecind_(counter, :) = indices
+      call self%add_entry_(indices)
    else
       at_mode_inc = at_mode + 1
-      call self%recursive_indices(counter, at_mode_inc, indices, currpop)
+      call self%recursive_indices_(at_mode_inc, indices, currpop)
    end if
 
    if (currpop >= self%populated_modes_) then
@@ -64,48 +69,75 @@ recursive subroutine recursive_indices(self, counter, at_mode, indices, currpop)
    currpop_inc = currpop + 1
    do i = 1, self%depth_ - sum(indices)
       indices_inc(at_mode) = i
-      if (at_mode >= self%modes_) then
-         counter = counter + 1
-         self%vecind_(counter, :) = indices_inc
+      if ((at_mode >= self%modes_)) then
+         call self%add_entry_(indices_inc)
       else
-         call self%recursive_indices(counter, at_mode_inc, indices_inc, &
-               currpop_inc)
+         call self%recursive_indices_(at_mode_inc, indices_inc, currpop_inc)
       end if
    end do
-end subroutine recursive_indices
+end subroutine recursive_indices_
 
 
-subroutine init(self, modes, depth, populated_modes)
+subroutine add_entry_(self, k)
+   implicit none
+   class(HStructure) :: self
+   integer, intent(in) :: k(self%modes_)
+
+   if (all(k <= self%cutoff_)) then
+      self%entries_ = self%entries_ + 1
+      self%vecind_(self%entries_, :) = k
+   end if
+end subroutine add_entry_
+
+subroutine init(self, modes, depth, populated_modes, cutoff)
    implicit none
    class(HStructure)             :: self
    integer, intent(in)           :: modes
    integer, intent(in)           :: depth
    integer, intent(in), optional :: populated_modes
+   integer, intent(in), optional :: cutoff(:)
+
    !----------------------------------------------------------------------------
-   integer i, j, counter, k(modes), currpop
+   integer i, j, max_entries, k(modes), currpop
 
    self%modes_ = modes
    self%depth_ = depth
    if (present(populated_modes)) then
-      self%populated_modes_ = populated_modes
+      if (populated_modes < 1) then
+         self%populated_modes_ = modes
+      else
+         self%populated_modes_ = populated_modes
+      endif
    else
       self%populated_modes_ = modes
    end if
 
+   allocate(self%cutoff_(modes))
+   if (present(cutoff)) then
+      do i = 1, modes
+         if (cutoff(i) < 0) then
+            self%cutoff_(i) = depth
+         else
+            self%cutoff_(i) = cutoff(i)
+         end if
+      end do
+   else
+      self%cutoff_ = depth
+   end if
+
    ! Calculate the maximum number of entries
-   self%entries_ = 0
+   max_entries = 0
    do i = 0, depth
-      self%entries_ = self%entries_ + choose(i + modes - 1, modes - 1)
+      max_entries = max_entries + choose(i + modes - 1, modes - 1)
    end do
 
-   call self%intind_%init(self%entries_ / BUCKETFILL + 1, modes)
-   allocate(self%vecind_(self%entries_, modes))
+   call self%intind_%init(max_entries / BUCKETFILL + 1, modes)
+   allocate(self%vecind_(max_entries, modes))
 
    k = 0
-   counter = 0
+   self%entries_ = 0
    currpop = 0
-   call self%recursive_indices(counter, 1, k, currpop)
-   self%entries_ = counter
+   call self%recursive_indices_(1, k, currpop)
 
    ! FIXME Resize vecind_
    allocate(self%indab_(self%entries_, self%modes_))
@@ -131,6 +163,7 @@ end subroutine init
 subroutine free(self)
    implicit none
    class(HStructure) :: self
+   call array_free(self%cutoff_)
    call array_free(self%indab_)
    call array_free(self%indbl_)
    call array_free(self%vecind_)
@@ -190,10 +223,14 @@ subroutine print(self)
    implicit none
    class(HStructure) :: self
    integer :: ind
-
+   print *, ''
+   print *, '------------------------------------------------------------------'
+   print *, 'Depth=', self%depth_, '  Populated Modes=', self%populated_modes_
+   print*, 'Cutoff=', self%cutoff_
    do ind = 1, self%entries_
       print *, ind, "=>", self%vecind_(ind, :)
    end do
+   print *, '------------------------------------------------------------------'
 end subroutine print
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
